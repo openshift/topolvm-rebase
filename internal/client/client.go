54f9375aece3b3f0380861f24c8984093432b865
Bulat Zamalutdinov <13433317+qJkee@users.noreply.github.com>
2026-03-25 13:35:34 +0400

    feat(deps): bump controller-runtime version

--- internal/client/client.go
+++ internal/client/client.go
@@ -377,6 +377,13 @@ func (c *wrappedSubResourceClient) Create(ctx context.Context, obj client.Object
 	return fmt.Errorf("wrappedSubResourceClient.Create is not implemented")
 }
 
+func (c *wrappedSubResourceClient) Apply(ctx context.Context, obj runtime.ApplyConfiguration, opts ...client.SubResourceApplyOption) error {
+	// - This method is currently not used in TopoLVM.
+	// - To implement this method, tests are required, but there are no similar tests
+	//   implemented in the upstream code, and we will need a lot of effort to study it.
+	return fmt.Errorf("wrappedSubResourceClient.Apply is not implemented")
+}
+
 func (c *wrappedSubResourceClient) Update(ctx context.Context, obj client.Object, opts ...client.SubResourceUpdateOption) error {
 	sc := c.client.SubResource(c.subResource)
 	gvk := obj.GetObjectKind().GroupVersionKind()
